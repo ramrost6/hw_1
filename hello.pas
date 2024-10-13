693de0e4c2e078d09bde71366c064e233cd8f8ee
Роман Морозов <ramrost666@gmail.com>
2024-10-13 14:53:32 +0300

    add hello_language

---
+++ hello.pas
@@ -0,0 +1,18 @@
+﻿begin
+  var lan := ReadString('Введите название языка(латиницей):');
+  case lan of
+    'Russian': PrintLn('Привет, мир!');
+    'Japanese': PrintLn('こんにちは、世界!');
+    'German': PrintLn('Hallo, Welt!');
+    'English': PrintLn('Hello, world!');
+  end;
+  
+end.
+{
+Введите название языка(латиницей): German
+Hallo, Welt!
+
+
+Введите название языка(латиницей): Japanese
+こんにちは、世界!
+}
